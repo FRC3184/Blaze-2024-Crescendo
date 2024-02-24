2e96db33c3b79a57e3bff801bb254e3872392e75
Seamus Boos <booss1@isd191.org>
2024-02-24 08:34:09 -0600

    Updating Branch to SeamusPath Planner

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -3,10 +3,16 @@ package frc.robot;
 // General Imports
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj.XboxController.Button;
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.RunCommand;
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
+
+import com.pathplanner.lib.auto.AutoBuilder;
+import com.pathplanner.lib.commands.PathPlannerAuto;
+
 import edu.wpi.first.math.MathUtil;
 // Shooter Imports
 // import frc.robot.TwoWheelShooterRevNeo.ShootCommand;
@@ -24,6 +30,8 @@ import frc.robot.constCrescendoBot.constMaxSwerveDrive.DriveConstants;
 
 public class RobotContainer {
 
+  private final SendableChooser<Command> autoChooser;
+
   // define subsystems5
   private final DriveSubsystemSwerve m_robotDrive = new DriveSubsystemSwerve();
   // private final ShooterSubsystem m_shooter = new ShooterSubsystem();
@@ -35,6 +43,15 @@ public class RobotContainer {
 
   public RobotContainer() {
     configureBindings();
+
+     // Build an auto chooser. This will use Commands.none() as the default option.
+    autoChooser = AutoBuilder.buildAutoChooser();
+
+    // Another option that allows you to specify the default auto by its name
+    // autoChooser = AutoBuilder.buildAutoChooser("My Default Auto");
+
+    SmartDashboard.putData("Auto Chooser", autoChooser);
+  
 
     // Configure default commands
     m_robotDrive.setDefaultCommand(
@@ -63,12 +80,14 @@ public class RobotContainer {
     new JoystickButton(m_driverController, Button.kLeftBumper.value).onTrue(new SetFastMode(m_robotDrive));
     new JoystickButton(m_driverController, Button.kLeftBumper.value).onFalse(new SetNormalMode(m_robotDrive));
 
+
     //GUNNER CONTROLS
     new JoystickButton(m_gunnerController, Button.kLeftBumper.value).whileTrue(new Intake(m_intake));
 
   }
 
   public Command getAutonomousCommand() {
-    return Commands.print("No autonomous command configured");
+    // return Commands.print("No autonomous command configured");
+    return autoChooser.getSelected();
   }
 }
