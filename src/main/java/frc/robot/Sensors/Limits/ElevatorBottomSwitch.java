2334f161bc034db403c9ddc01182a0e9b7d30aa6
Inconn <62788103+Inconn@users.noreply.github.com>
2024-03-27 20:12:36 -0500

    add limitsensor subsystem
    remove invert since it doesn't seem to be needed
    
    move limit sensor to mechanisms, made placeholder limit switch thing

---
+++ src/main/java/frc/robot/Sensors/Limits/ElevatorBottomSwitch.java
@@ -0,0 +1,11 @@
+package frc.robot.Sensors.Limits;
+
+import frc.Mechanisms.sensorTypes.LimitSwitch;
+
+public class ElevatorBottomSwitch extends LimitSwitch {
+    public ElevatorBottomSwitch() {
+        // made as placeholder but prob can just be used
+        // make sure port and label is correct if you dupe this file
+        super(0, "elevator bottom");
+    }
+}
