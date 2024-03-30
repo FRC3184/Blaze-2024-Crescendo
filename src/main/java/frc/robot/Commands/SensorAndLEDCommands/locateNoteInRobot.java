e04dcb22e6425dd406ff0efce09a64ac620f8a40
Seamus Boos <booss1@isd191.org>
2024-03-30 14:10:57 -0500

    LEDs work and change according to ODS reading. Location outputs to the smart dashboard.

--- src/main/java/frc/robot/Commands/SensorAndLEDCommands/locateNoteInRobot.java
+++ src/main/java/frc/robot/Commands/SensorAndLEDCommands/locateNoteInRobot.java
@@ -1,8 +1,10 @@
 package frc.robot.Commands.SensorAndLEDCommands;
 
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.Sensors.ODS.CarriageODS;
 import frc.robot.Sensors.ODS.IntakeODS;
+import frc.robot.SubmoduleSubsystemConstants.ConstLEDs.COLORS;
 import frc.robot.Subsystems.LEDs;
 
 public class locateNoteInRobot extends Command {
@@ -29,10 +31,10 @@ public class locateNoteInRobot extends Command {
     @Override
     public void execute() {
         //Locate the note and update the location and if the robot has a note
-        if(intakeODS.getSightStatus()){
+        if((intakeODS.getSightStatus()&&carriageODS.getSightStatus())||carriageODS.getSightStatus()){
             hasNote = true;
             noteLocation = "Intake";
-        } else if ((intakeODS.getSightStatus()&&carriageODS.getSightStatus())||carriageODS.getSightStatus()){
+        } else if (intakeODS.getSightStatus()){
             hasNote = true;
             noteLocation = "Carriage";
         } else {
@@ -42,12 +44,21 @@ public class locateNoteInRobot extends Command {
 
         // Change the LEDs based on note posession
         if (hasNote){
-            new LEDsOrange(leds);
+        leds.orangeWave();
+        leds.setLeds();
+        leds.UpdateLedMode("Orange Wave");
         }
         else {
-            new LEDsWhite(leds);
+        leds.setLedBufferByGroup(0, leds.getLedLength(), COLORS.WHITE);  
+        leds.setLeds();
+        leds.UpdateLedMode("White");
         }
+        dashboartOut();
     }
+
+        void dashboartOut(){
+            SmartDashboard.putString("noteLocation", noteLocation);
+        }
 
     @Override
     public void end(boolean interrupted) {
