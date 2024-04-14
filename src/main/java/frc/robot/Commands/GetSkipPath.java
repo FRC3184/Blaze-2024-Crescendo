ab7aed16433a36a0cd2c76ad321117cb101ca602
Seamus Boos <booss1@isd191.org>
2024-04-13 20:31:51 -0500

    Fixed a potential issue with path skipping without using auto limelight intake command first

--- src/main/java/frc/robot/Commands/GetSkipPath.java
+++ src/main/java/frc/robot/Commands/GetSkipPath.java
@@ -24,7 +24,7 @@ public class GetSkipPath extends Command{
 
     @Override
     public void end(boolean interrupted){
-
+        ConstAuto.skipNextPath = false;
     }
 
     private void doNothing(){
